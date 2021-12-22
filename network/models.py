206b8e236c349f0c6862a642f1ef8a689dc6cbbd
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-23 00:45:18 +0100

    add Post model

--- network/models.py
+++ network/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=240)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
