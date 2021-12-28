a9cb82120e57dc099aa5ef736fc58f65ca014573
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-28 13:23:07 +0100

    register models

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    User,
+    Post,
+    Follower,
+)
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Follower)
