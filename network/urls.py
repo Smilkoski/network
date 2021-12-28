5d9af9017e5557b33d0d7fa9d8c1c794d89ff6ae
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-28 13:21:29 +0100

    add routes for user detail, follow, unfollow

--- network/urls.py
+++ network/urls.py
@@ -4,11 +4,17 @@ from .views import (
     ListAppendView,
     login_view,
     logout_view,
-    register
+    register,
+    UserDetailView,
+    follow,
+    unfollow,
 )
 
 urlpatterns = [
     path("", ListAppendView.as_view(), name="index"),
+    path("detail/<int:pk>/", UserDetailView.as_view(), name="user-detail"),
+    path("follow/<int:pk1>/<int:pk2>/", follow, name="follow"),
+    path("unfollow/<int:pk1>/<int:pk2>/", unfollow, name="unfollow"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("register/", register, name="register"),
