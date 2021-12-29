a53378404b7891e1bb8c14ce85c1e4655a167822
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-29 02:53:09 +0100

    add routes for post detail and api

--- network/urls.py
+++ network/urls.py
@@ -9,15 +9,20 @@ from .views import (
     follow,
     unfollow,
     FollowingListView,
+    PostDetailView,
+    post,
 )
 
 urlpatterns = [
     path("", ListAppendView.as_view(), name="index"),
     path("detail/<int:pk>/", UserListView.as_view(), name="user-detail"),
+    path("post_detail/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("follow/<int:pk1>/<int:pk2>/", follow, name="follow"),
     path("unfollow/<int:pk1>/<int:pk2>/", unfollow, name="unfollow"),
     path("following/", FollowingListView.as_view(), name="following"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("register/", register, name="register"),
+    # API Routes
+    path("post/<int:post_id>", post, name="get_post"),
 ]
